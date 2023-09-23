5ff5cdff8906fbb6843e74de469c44119e81b5a0
Gaurav Bhattacharjee <grvb1515@gmail.com>
2023-09-23 20:01:50 +0530

    Modify script to add alias for copilot

--- script.sh
+++ script.sh
@@ -17,7 +17,23 @@ source "$venv_name/bin/activate"
 
 pip3 install -r requirements.txt
 
-
-python3 main.py
-
 deactivate
+
+script_name="main.py"
+
+current_dir=$(pwd)
+
+bashrc_path="$HOME/.bashrc"
+zshrc_path="$HOME/.zshrc"
+
+echo "alias ghost=\"source $current_dir/$venv_name/bin/activate && python $current_dir/$script_name && deactivate\" " >> "$bashrc_path"
+
+# Add the alias to .zshrc
+echo "alias ghost=\"source $current_dir/$venv_name/bin/activate && python $current_dir/$script_name && deactivate\"" >> "$zshrc_path"
+
+# Source the configuration files to apply the alias immediately
+source "$bashrc_path"
+source "$zshrc_path"
+
+# Inform the user
+echo "ghost has been set up successfully"
